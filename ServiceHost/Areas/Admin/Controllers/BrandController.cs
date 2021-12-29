d8b2f31e268a2bf806b81cacb474ed71cae630c9
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-12-29 08:31:20 +0330

    Complete Brand Service

---
+++ ServiceHost/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,58 @@
+﻿using Marketer.Application.Contract.AI.Products;
+using Marketer.Application.Contract.ViewModels.Products;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ServiceHost.Areas.Admin.Controllers
+{
+    public class BrandController : AdminBaseController
+    {
+        private readonly IBrandApplication _brandApplication;
+
+        public BrandController(IBrandApplication categoryApplication) => _brandApplication = categoryApplication;
+
+        public async Task<IActionResult> Index() => View(await _brandApplication.GetAll());
+
+        [HttpGet]
+        public IActionResult Create() => PartialView();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateBrandVM command)
+        {
+            var result = await _brandApplication.Create(command);
+
+            if (result.IsSucceeded) TempData[SuccessMessage] = result.Message;
+
+            return new JsonResult(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(long id) => PartialView(await _brandApplication.GetDetailForEditBy(id));
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(EditBrandVM command)
+        {
+            var result = await _brandApplication.Edit(command);
+
+            if (result.IsSucceeded) TempData[SuccessMessage] = result.Message;
+
+            return new JsonResult(result);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(long id) => PartialView(id);
+
+        [ActionName("Delete")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> PostDelete(long id)
+        {
+            var result = await _brandApplication.Delete(id);
+
+            if (result.IsSucceeded) TempData[SuccessMessage] = result.Message;
+
+            return new JsonResult(result);
+        }
+    }
+}
