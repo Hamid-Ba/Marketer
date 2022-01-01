e1da608671a1431a30cacaf388a90a8e4ddf3ca1
hamid balalzadeh <balalzadehhamid79@gmail.com>
2022-01-01 11:54:11 +0330

    Inject Visitor Panel

---
+++ ServiceHost/Areas/Visitor/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Areas.Visitor.Controllers
+{
+    public class DashboardController : VisitorBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
