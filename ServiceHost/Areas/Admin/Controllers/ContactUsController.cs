6f52b55f1164cb6227d4a1333d11d151d675bb6e
hamid balalzadeh <balalzadehhamid79@gmail.com>
2022-01-02 11:34:19 +0330

    Complete ContactUs Service In Admin Panel

---
+++ ServiceHost/Areas/Admin/Controllers/ContactUsController.cs
@@ -0,0 +1,19 @@
+﻿using Marketer.Application.Contract.AI.Extera;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ServiceHost.Areas.Admin.Controllers
+{
+    public class ContactUsController : AdminBaseController
+    {
+        private readonly IContactUsApplication _contactUsApplication;
+
+        public ContactUsController(IContactUsApplication contactUsApplication) => _contactUsApplication = contactUsApplication;
+
+        public async Task<IActionResult>  Index() => View(await _contactUsApplication.GetAll());
+
+        [HttpGet]
+        public async Task<IActionResult> Detail(long id) => PartialView(await _contactUsApplication.GetMessageBy(id));
+        
+    }
+}
