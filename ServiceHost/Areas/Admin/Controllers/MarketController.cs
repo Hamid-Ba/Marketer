2e1b3b270457f300d0281cdf6ebc94e5bdcf5cfe
hamid balalzadeh <balalzadehhamid79@gmail.com>
2022-01-02 11:18:58 +0330

    Add Markets To Admin Panel

---
+++ ServiceHost/Areas/Admin/Controllers/MarketController.cs
@@ -0,0 +1,16 @@
+﻿using Marketer.Application.Contract.AI.Products;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ServiceHost.Areas.Admin.Controllers
+{
+    public class MarketController : AdminBaseController
+    {
+        private readonly IMarketApplication _marketApplication;
+
+        public MarketController(IMarketApplication marketApplication) => _marketApplication = marketApplication;
+
+        public async Task<IActionResult> Index() => View(await _marketApplication.GetAll());
+        
+    }
+}
