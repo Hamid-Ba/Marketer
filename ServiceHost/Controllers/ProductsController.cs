ac6862726de50dc78bb09f067deccd3969488dff
hamid balalzadeh <balalzadehhamid79@gmail.com>
2022-01-03 17:55:09 +0330

    Complete Products Page

--- ServiceHost/Controllers/ProductsController.cs
+++ ServiceHost/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
 ﻿using Framework.Application;
+using Marketer.Query.Queries.Brands;
+using Marketer.Query.Queries.Categories;
 using Marketer.Query.Queries.Products;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
@@ -9,16 +11,23 @@ namespace ServiceHost.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly IBrandQuery _brandQuery;
         private readonly IProductQuery _productQuery;
+        private readonly ICategoryQuery _categoryQuery;
 
-        public ProductsController(IProductQuery productQuery)
+        public ProductsController(IBrandQuery brandQuery, IProductQuery productQuery, ICategoryQuery categoryQuery)
         {
+            _brandQuery = brandQuery;
             _productQuery = productQuery;
+            _categoryQuery = categoryQuery;
         }
 
         public async Task<IActionResult> Index(ProductSort sortExp, string search, int pageIndex = 1)
         {
             var products = await _productQuery.GetAll(sortExp,search);
+
+            ViewBag.Brands = await _brandQuery.GetAll();
+            ViewBag.Categories = await _categoryQuery.GetAll();
 
             var model = PagingList.Create(products, 6, pageIndex);
 
